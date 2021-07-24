93b9b5634d268371ed6df03a117eace96e1e3108
Loki Wijnen <github@kloki.com>
2021-07-24 15:11:20 +0200

    implement 404 error page

--- project/pages/routes.py
+++ project/pages/routes.py
@@ -5,3 +5,11 @@ from flask import render_template
 @pages_blueprint.route('/')
 def home():
     return render_template('pages/home.html')
+
+
+# This is specifically for netlify.com which is where the demo is currently hosted.
+# Your hosting provider might need error pages handled differently.
+# Please see the netlify.toml file in the root of the project for more info.
+@pages_blueprint.route('/404/')
+def static_error_404():
+    return render_template('404.html')
